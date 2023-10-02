5149c296b8ac097f57df4e3f93e84c7f8d46a6ca
Alain72 <Alain.vangaalen@gmail.com>
2023-10-02 21:13:34 +0200

    release build

--- Agw.Generic/Properties/AssemblyInfo.cs
+++ Agw.Generic/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AvgW.Generic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Avg Works")]
 [assembly: AssemblyProduct("AvgW.Generic")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
